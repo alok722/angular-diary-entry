d751ad4c7fa8bb984cdbfae39332485bff091c05
Alok Raj <alokr417@gmail.com>
2021-01-29 15:46:28 +0530

    header footer template done

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  email = null;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {
+    auth.getUser().subscribe((user) => {
+      console.log('✔ logged user details:', user);
+      this.email = user?.email;
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  async handleSignOut() {
+    try {
+      await this.auth.signOut();
+      this.router.navigateByUrl('/signin');
+      this.toastr.info('😃 Logout success!');
+      this.email = null;
+    } catch (error) {
+      console.error(error);
+      this.toastr.error('😧 Error Signing out !!!');
+    }
   }
 
 }
