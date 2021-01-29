23420433894053dd1d2fdc28e44d61fecb0e49dc
Alok Raj <alokr417@gmail.com>
2021-01-29 19:45:55 +0530

    sign-in component coded

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private toastr: ToastrService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(f: NgForm) {
+    const { email, password } = f.form.value;
+
+    this.auth.signIn(email, password)
+      .then((res) => {
+        this.toastr.info('😃 SignIn success!', '', {
+          closeButton: true,
+        });
+        this.router.navigateByUrl('/');
+      })
+      .catch((err) => {
+        console.error(err.message);
+        this.toastr.error('😧 Error Signing in !!!', '', {
+          closeButton: true,
+        });
+      });
   }
 
 }
