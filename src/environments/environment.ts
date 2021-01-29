a625555337d914504eaca903f5eb5b9cd7e5c131
Alok Raj <alokr417@gmail.com>
2021-01-29 15:26:12 +0530

    firebase auth service developed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: "AIzaSyCFs9oDVOt5JdOAXQ3sZOgNL-7X_ptTpI8",
+    authDomain: "diaryentry722.firebaseapp.com",
+    databaseURL: "https://diaryentry722-default-rtdb.firebaseio.com",
+    projectId: "diaryentry722",
+    storageBucket: "diaryentry722.appspot.com",
+    messagingSenderId: "669765127705",
+    appId: "1:669765127705:web:4b4d93596383277e1b6488",
+    measurementId: "G-T2J08MLH2M"
+  }
 };
 
 /*
