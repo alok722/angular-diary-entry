0af8551f19183d7195a3017bf63c3d85ebce7785
Alok Raj <alokr417@gmail.com>
2021-01-31 18:55:37 +0530

    deployable changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,14 @@ export const environment = {
   production: true,
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   firebaseConfig: {
-    apiKey: "AIzaSyCFs9oDVOt5JdOAXQ3sZOgNL-7X_ptTpI8",
-    authDomain: "diaryentry722.firebaseapp.com",
-    databaseURL: "https://diaryentry722-default-rtdb.firebaseio.com",
-    projectId: "diaryentry722",
-    storageBucket: "diaryentry722.appspot.com",
-    messagingSenderId: "669765127705",
-    appId: "1:669765127705:web:4b4d93596383277e1b6488",
-    measurementId: "G-T2J08MLH2M"
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGE_SENDER_ID,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENT_ID
   }
 };
 
